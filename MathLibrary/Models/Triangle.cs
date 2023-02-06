24611b42454afd5d89cd5dca6b996ee490f63f1d
akiri <akiri@DESKTOP-30M2ICJ>
2023-02-06 11:06:46 +0300

    Add project files.

---
+++ MathLibrary/Models/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary.Models
+{
+	public class Triangle : Figure
+	{
+		public override double GetArea()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
